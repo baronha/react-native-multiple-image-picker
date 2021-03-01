64fa884a1e58a1a9883aae2806cd7d3d5344fa18
Bao <baobaovabao@gmail.com>
2021-03-01 22:31:33 +0700

    publish 0.2.1

--- example/src/App.js
+++ example/src/App.js
@@ -12,6 +12,7 @@ export default function App() {
       const response = await MultipleImagePicker.openPicker({
         selectedAssets: images,
         isExportThumbnail: true,
+        // maxVideo: 1,
         // selectedColor: '#f9813a',
       });
       console.log('done: ', response);
